695298418dbb88e3c5182a146b4666a79338ffbb
Christopher Tarquini <chris+github@tarq.io>
2021-03-22 21:40:15 +0000

    Lesson 2

--- js/app.js
+++ js/app.js
@@ -4,4 +4,31 @@ import config from './config.js';
 import Runner from './runner.js';
 
 // Press Run to start the game!
-const runner = new Runner('.interstitial-wrapper', config);
+
+// Copy the Client ID for your environment from https://app.launchdarkly.com/settings/projects
+const LD_CLIENT_ID = '<YOUR CLIENT ID HERE>';
+
+// LDClient is loaded by a script tag in index.html
+// Let's initialize it using our
+const ldclient = LDClient.initialize(LD_CLIENT_ID, {"anonymous": true});
+
+// This function will be called once the LaunchDarkly client has initialized
+function startGame() {  
+  // Before we start the game, let's check space mode is enabled for this user
+  // We will use `false` as the fallback value, so that if the client fails to initialize or the flag has not yet been created, the dinosaur sprite sheet will be used.
+  if (ldclient.variation("space-mode", false)) {
+    // Locate the image elements in the document by their ID
+    // You can see these elements defined in index.html
+    const smallSprites = document.getElementById("resources-1x");
+    const largeSprites = document.getElementById("resources-2x");
+    // Change the source url used for the sprite sheets 
+    smallSprites.src = "images/100-percent/100-sprite-space.png";
+    largeSprites.src = "images/200-percent/200-sprite-space.png";
+  }
+  // Start the game!
+  const runner = new Runner('.interstitial-wrapper', config);
+}
+
+// Register an event handler that calls `startGame` once the client is ready
+ldclient.on('ready', startGame);
+
