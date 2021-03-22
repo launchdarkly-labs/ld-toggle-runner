e76f5c0ae2825cd61027e9078a891a9d0cf80c44
Christopher Tarquini <chris+github@tarq.io>
2021-03-22 21:32:09 +0000

    Lesson 1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 
+// Follow the instructions here: https://learn-launchdarkly.netlify.app/
 import config from './config.js';
 import Runner from './runner.js';
 
+// Press Run to start the game!
 const runner = new Runner('.interstitial-wrapper', config);
